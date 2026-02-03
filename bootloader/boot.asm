122f6adcec0028bd785ebf13c8906b0ec6167b79
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-03 23:34:24 +0530

    Implement disk loader and error handling in boot.asm
    Added disk loading functionality and error handling to bootloader.

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,3 +1,5 @@
+#arshan
+
 [org 0x7c00]
 
 mov ah, 0x0e
@@ -22,6 +24,7 @@ dw 0xaa55
 
 
 
+#hello this is arshan's os!
 
 [org 0x7c00]
 
@@ -43,3 +46,71 @@ message db "Hello, this is Arshan's OS!", 0
 times 510-($-$$) db 0
 dw 0xaa55
 
+
+
+
+
+#disk loader function
+
+[org 0x7c00]
+
+mov [BOOT_DRIVE], dl ; 1. BIOS stores the boot drive number in DL. Save it.
+
+mov bp, 0x8000       ; 2. Set the stack safely away from our code
+mov sp, bp
+
+mov bx, 0x9000       ; 3. Load 5 sectors to memory address 0x9000
+mov dh, 1            ; (Read 1 sector)
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+mov bx, 0x9000       ; 4. Jump to the address where we loaded the kernel
+jmp bx
+
+jmp $
+
+; --- DISK LOAD FUNCTION ---
+disk_load:
+    push dx          ; Store DX on stack so we can check it later
+
+    mov ah, 0x02     ; BIOS read sector function
+    mov al, dh       ; Read DH sectors
+    mov ch, 0x00     ; Select Cylinder 0
+    mov dh, 0x00     ; Select Head 0
+    mov cl, 0x02     ; Start reading from Sector 2 (Sector 1 is bootloader)
+    
+    int 0x13         ; BIOS interrupt
+
+    jc disk_error    ; "Jump Carry". If error, BIOS sets Carry Flag.
+
+    pop dx           ; Restore DX from the stack
+    cmp dh, al       ; BIOS sets AL to # of sectors actually read
+    jne disk_error   ; If AL != DH (sectors requested), error!
+    ret
+
+disk_error:
+    mov bx, DISK_ERROR_MSG
+    call print_string
+    jmp $
+
+; --- PRINT STRING FUNCTION (From Day 2) ---
+print_string:
+    pusha
+    mov ah, 0x0e
+.loop:
+    mov al, [bx]
+    cmp al, 0
+    je .done
+    int 0x10
+    inc bx
+    jmp .loop
+.done:
+    popa
+    ret
+
+; --- DATA ---
+BOOT_DRIVE: db 0
+DISK_ERROR_MSG: db "Disk read error!", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
