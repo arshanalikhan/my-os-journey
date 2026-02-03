8c0eb02c371a31a9b5fd18b9b0b707549c6875e4
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-03 23:35:17 +0530

    Add initial kernel assembly code with print function

---
+++ kernel/kernel.asm
@@ -0,0 +1,22 @@
+[org 0x9000]          ; tell NASM where this code will live in memory
+
+jmp main
+
+main:
+    mov bx, KERNEL_MSG
+    call print_string_kernel
+    jmp $
+
+print_string_kernel:  ; Simple print function for the kernel
+    mov ah, 0x0e
+.loop:
+    mov al, [bx]
+    cmp al, 0
+    je .done
+    int 0x10
+    inc bx
+    jmp .loop
+.done:
+    ret
+
+KERNEL_MSG: db "Successfully landed in the Kernel!", 0
