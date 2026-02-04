74e2a157339dfa60496a2ee456e4e745fdb1bb92
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-04 23:00:58 +0530

    Add print_string_pm function for string output
    Implement a function to print strings in protected mode using video memory.

--- protected mode/print_string_pm.asm
+++ protected mode/print_string_pm.asm
@@ -1,1 +1,25 @@
+[bits 32] ; Tell NASM this is 32-bit code
 
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
+
+print_string_pm:
+    pusha
+    mov edx, VIDEO_MEMORY ; Set EDX to the start of video memory
+
+print_string_pm_loop:
+    mov al, [ebx]          ; Store the char at EBX in AL
+    mov ah, WHITE_ON_BLACK ; Store the attributes in AH
+
+    cmp al, 0
+    je print_string_pm_done
+
+    mov [edx], ax          ; Store char and attributes at current character cell
+    add ebx, 1             ; Increment EBX to the next char in string
+    add edx, 2             ; Move to next character cell in video memory
+
+    jmp print_string_pm_loop
+
+print_string_pm_done:
+    popa
+    ret
