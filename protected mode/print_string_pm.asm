bbc3c155e77f124a661a8d65aba47ec1f7718ded
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-04 23:03:10 +0530

    Refactor print_string_pm to use BIOS interrupt

--- protected mode/print_string_pm.asm
+++ protected mode/print_string_pm.asm
@@ -1,25 +1,13 @@
-[bits 32] ; Tell NASM this is 32-bit code
-
-VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
-
-print_string_pm:
+print_string:
     pusha
-    mov edx, VIDEO_MEMORY ; Set EDX to the start of video memory
-
-print_string_pm_loop:
-    mov al, [ebx]          ; Store the char at EBX in AL
-    mov ah, WHITE_ON_BLACK ; Store the attributes in AH
-
+    mov ah, 0x0e
+.loop:
+    mov al, [bx]
     cmp al, 0
-    je print_string_pm_done
-
-    mov [edx], ax          ; Store char and attributes at current character cell
-    add ebx, 1             ; Increment EBX to the next char in string
-    add edx, 2             ; Move to next character cell in video memory
-
-    jmp print_string_pm_loop
-
-print_string_pm_done:
+    je .done
+    int 0x10
+    inc bx
+    jmp .loop
+.done:
     popa
     ret
