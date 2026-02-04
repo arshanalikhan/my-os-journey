61b0958bfe9f3f6fc0871a155c5f2d210906c552
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-04 23:00:13 +0530

    Define Global Descriptor Table in gdt.asm

--- protected mode/gdt.asm
+++ protected mode/gdt.asm
@@ -1,1 +1,37 @@
+; gdt.asm - Global Descriptor Table
+gdt_start:
 
+gdt_null: ; 1. The mandatory null descriptor
+    dd 0x0
+    dd 0x0
+
+gdt_code: ; 2. The Code Segment descriptor
+    ; base=0x0, limit=0xfffff, 1st flags: (present)1 (privilege)00 (descriptor type)1 -> 1001b
+    ; type flags: (code)1 (conforming)0 (readable)1 (accessed)0 -> 1010b
+    ; 2nd flags: (granularity)1 (32-bit default)1 (64-bit seg)0 (AVL)0 -> 1100b
+    dw 0xffff    ; Limit (bits 0-15)
+    dw 0x0       ; Base (bits 0-15)
+    db 0x0       ; Base (bits 16-23)
+    db 10011010b ; 1st flags, type flags
+    db 11001111b ; 2nd flags, Limit (bits 16-19)
+    db 0x0       ; Base (bits 24-31)
+
+gdt_data: ; 3. The Data Segment descriptor
+    ; Same as code segment except for the type flags:
+    ; type flags: (code)0 (expand down)0 (writable)1 (accessed)0 -> 0010b
+    dw 0xffff    ; Limit (bits 0-15)
+    dw 0x0       ; Base (bits 0-15)
+    db 0x0       ; Base (bits 16-23)
+    db 10010010b ; 1st flags, type flags
+    db 11001111b ; 2nd flags, Limit (bits 16-19)
+    db 0x0       ; Base (bits 24-31)
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; Size of GDT
+    dd gdt_start               ; Start address of GDT
+
+; Constants for the GDT segment offsets
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
