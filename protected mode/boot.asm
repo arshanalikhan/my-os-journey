117865e25434d009bdcb296c3fa6df9f25ccbff9
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-04 23:02:20 +0530

    Implement boot sequence for protected mode

--- protected mode/boot.asm
+++ protected mode/boot.asm
@@ -1,1 +1,34 @@
+[org 0x7c00]
 
+mov [BOOT_DRIVE], dl ; Save the boot drive
+
+mov bp, 0x9000       ; Set the stack
+mov sp, bp
+
+mov bx, MSG_REAL_MODE
+call print_string
+
+call switch_to_pm    ; Note: We never return from here
+
+jmp $
+
+%include "gdt.asm"
+%include "print_string.asm"    ; Use your old 16-bit print file (create this if needed or paste code here)
+%include "switch_to_pm.asm"
+%include "print_string_pm.asm"
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm   ; Use our new 32-bit print function
+
+    jmp $                  ; Hang here for today
+
+; Global variables
+BOOT_DRIVE: db 0
+MSG_REAL_MODE: db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE: db "Successfully landed in 32-bit Protected Mode", 0
+
+; Padding
+times 510-($-$$) db 0
+dw 0xaa55
