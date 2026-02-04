7305dbc2d56d5edae54d4865f4e319e2c9ac6482
Arshan Ali Khan <arshanalikhan123@gmail.com>
2026-02-04 23:03:47 +0530

    Update switch_to_pm.asm

--- protected mode/switch_to_pm.asm
+++ protected mode/switch_to_pm.asm
@@ -1,13 +1,13 @@
 [bits 16]
 switch_to_pm:
-    cli                    ; 1. Disable interrupts (BIOS is dead to us now)
+    cli                    ; 1. Disable interrupts
     lgdt [gdt_descriptor]  ; 2. Load the GDT descriptor
 
-    mov eax, cr0           ; 3. Set the first bit of Control Register 0 (CR0)
-    or eax, 0x1
+    mov eax, cr0
+    or eax, 0x1            ; 3. Set 32-bit mode bit in CR0
     mov cr0, eax
 
-    jmp CODE_SEG:init_pm   ; 4. Far jump to 32-bit code to flush the CPU pipeline
+    jmp CODE_SEG:init_pm   ; 4. Far jump by using a different segment
 
 [bits 32]
 init_pm:
